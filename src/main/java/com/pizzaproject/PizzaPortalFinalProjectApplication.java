fab1fa3d1482181cb99f61c41893721767d28991
Przemek <pzawodzinski@gmail.com>
2023-04-15 12:52:41 +0200

    Create project

---
+++ src/main/java/com/pizzaproject/PizzaPortalFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pizzaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPortalFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaPortalFinalProjectApplication.class, args);
+	}
+
+}

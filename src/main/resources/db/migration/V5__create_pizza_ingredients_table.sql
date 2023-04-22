e3990fe049c0f39b880f9239bc6e4f93687ca141
dandrozd88 <113719229+dandrozd88@users.noreply.github.com>
2023-04-22 10:40:04 +0200

    Delete V5__create_pizza_ingredients_table.sql

--- src/main/resources/db/migration/V5__create_pizza_ingredients_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS pizza_ingredients (
-    pizza_id BIGINT NOT NULL,
-    ingredient_id BIGINT NOT NULL,
-    PRIMARY KEY (pizza_id, ingredient_id),
-    FOREIGN KEY (pizza_id) REFERENCES pizzas (id),
-    FOREIGN KEY (ingredient_id) REFERENCES ingredients (id)
-    );

284c079474f2ef2bb298a28416ca9778ac3ff393
Daniel Drozdowski <drozdowski88@gmail.com>
2023-04-22 15:52:42 +0200

    Add import for OrderStatus

--- src/main/java/com/pizzaproject/model/Order.java
+++ src/main/java/com/pizzaproject/model/Order.java
@@ -3,6 +3,7 @@ package com.pizzaproject.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import com.pizzaproject.enums.OrderStatus;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;

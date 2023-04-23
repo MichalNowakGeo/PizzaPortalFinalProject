066040ed1d1fa592ee79529ec142bfd591fb4703
Daniel Drozdowski <drozdowski88@gmail.com>
2023-04-23 10:34:25 +0200

    Fix bugs in MainController, Order, OrderService, Pizza, PizzaService. Add exception package and ResourceNotException.java

--- src/main/java/com/pizzaproject/controller/MainController.java
+++ src/main/java/com/pizzaproject/controller/MainController.java
@@ -1,18 +1,27 @@
 package com.pizzaproject.controller;
 
 
+import com.pizzaproject.model.Pizza;
+import com.pizzaproject.service.PizzaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MainController {
 
-    @GetMapping("/api/greetings")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<String> helloForAdmin() {
-        return ResponseEntity.ok("Hello Admin");
+    private final PizzaService pizzaService;
+
+    @GetMapping("/api/pizza/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Pizza getPizza(@PathVariable Long id) {
+        return pizzaService.getPizzaById(id);
     }
 
     @GetMapping("/hello")
